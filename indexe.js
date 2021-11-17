3a3817b16363b79647f63c66a95fa43da8b672df
charliesouthview <94203256+charliesouthview@users.noreply.github.com>
2021-11-16 21:10:50 -0600

    Update and rename index.js to indexe.js

---
+++ indexe.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+import Chats from "./index"
+import Login from "./index"
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Avenir' }}>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/chats" component={Chats} />
+            <Route path="/" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
+  )
+}
+
+export default App
