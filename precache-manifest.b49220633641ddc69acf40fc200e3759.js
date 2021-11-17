86c40a3a81aab548e6daf8e6a9320abfa2d0fe45
charliesouthview <94203256+charliesouthview@users.noreply.github.com>
2021-11-17 01:59:50 -0600

    Add files via upload

---
+++ precache-manifest.b49220633641ddc69acf40fc200e3759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eae5b2661ba02853ea1d4dcaf9607c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e0a867c5dfad8c5931",
+    "url": "/static/css/main.3cdfab06.chunk.css"
+  },
+  {
+    "revision": "fee5cc19e6fac39ef2b5",
+    "url": "/static/js/2.abacbbcb.chunk.js"
+  },
+  {
+    "revision": "d6431fbb82d158dc84c621f958b05b6a",
+    "url": "/static/js/2.abacbbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e0a867c5dfad8c5931",
+    "url": "/static/js/main.df4493f5.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
